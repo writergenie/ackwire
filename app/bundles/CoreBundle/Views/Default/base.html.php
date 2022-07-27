1dffcdc656e29b54eea3827de900c595bc151e20
Sarang Sharma <sarang.sharma14@gmail.com>
2022-07-27 21:31:28 +0530

    manual whitelabeler changes

--- app/bundles/CoreBundle/Views/Default/base.html.php
+++ app/bundles/CoreBundle/Views/Default/base.html.php
@@ -42,10 +42,8 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-xs-6 text-muted"><?php echo $view['translator']->trans('mautic.core.copyright', ['%date%' => date('Y')]); ?></div>
-                        <div class="col-xs-6 text-muted text-right small">v<?php
-                            /** @var \Mautic\CoreBundle\Templating\Helper\VersionHelper $version */
-                            $version = $view['version'];
-                            echo $version->getVersion(); ?>
+                        <div class="col-xs-6 text-muted text-right small">
+                             &copy; 2022 Ackwire 
                         </div>
                     </div>
                 </div>
